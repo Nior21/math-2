102ba021be9204f749e08ccd7dd6a687ec90bbc5
Nior <52194934+Nior21@users.noreply.github.com>
2023-11-24 18:27:04 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,219 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "1":return img`
+. . 1 . . 
+. 1 1 . . 
+. . 1 . . 
+. . 1 . . 
+. . 1 . . 
+. . 1 . . 
+. 1 1 1 . 
+`;
+            case "image2":
+            case "0":return img`
+. 1 1 1 . 
+1 . . . 1 
+1 . . . 1 
+1 . . . 1 
+1 . . . 1 
+1 . . . 1 
+. 1 1 1 . 
+`;
+            case "image3":
+            case "2":return img`
+1 1 1 1 1 
+. . . . 1 
+. . . . 1 
+1 1 1 1 1 
+1 . . . . 
+1 . . . . 
+1 1 1 1 1 
+`;
+            case "image4":
+            case "3":return img`
+1 1 1 1 . 
+. . . . 1 
+. . . . 1 
+1 1 1 1 . 
+. . . . 1 
+. . . . 1 
+1 1 1 1 . 
+`;
+            case "image5":
+            case "4":return img`
+1 . . . 1 
+1 . . . 1 
+1 . . . 1 
+1 1 1 1 1 
+. . . . 1 
+. . . . 1 
+. . . . 1 
+`;
+            case "image6":
+            case "5":return img`
+1 1 1 1 1 
+1 . . . . 
+1 . . . . 
+1 1 1 1 1 
+. . . . 1 
+. . . . 1 
+1 1 1 1 1 
+`;
+            case "image7":
+            case "6":return img`
+1 1 1 1 1 
+1 . . . . 
+1 . . . . 
+1 1 1 1 1 
+1 . . . 1 
+1 . . . 1 
+1 1 1 1 1 
+`;
+            case "image8":
+            case "7":return img`
+1 1 1 1 1 
+. . . . 1 
+. . . 1 . 
+. . 1 . . 
+. 1 . . . 
+. 1 . . . 
+. 1 . . . 
+`;
+            case "image9":
+            case "8":return img`
+1 1 1 1 1 
+1 . . . 1 
+1 . . . 1 
+1 1 1 1 1 
+1 . . . 1 
+1 . . . 1 
+1 1 1 1 1 
+`;
+            case "image10":
+            case "9":return img`
+1 1 1 1 1 
+1 . . . 1 
+1 . . . 1 
+1 1 1 1 1 
+. . . . 1 
+. . . . 1 
+1 1 1 1 1 
+`;
+            case "image13":
+            case "Sign2":return img`
+. . . . . 
+7 . . . 7 
+. 7 . 7 . 
+. . 7 . . 
+. 7 . 7 . 
+7 . . . 7 
+. . . . . 
+`;
+            case "image14":
+            case "Sign3":return img`
+. . . . . 
+. . 7 . . 
+. . . . . 
+7 7 7 7 7 
+. . . . . 
+. . 7 . . 
+. . . . . 
+`;
+            case "image12":
+            case "Sign1":return img`
+. . . . . 
+. . . . . 
+. . . . . 
+7 7 7 7 7 
+. . . . . 
+. . . . . 
+. . . . . 
+`;
+            case "image11":
+            case "Sign0":return img`
+. . . . . 
+. . 7 . . 
+. . 7 . . 
+7 7 7 7 7 
+. . 7 . . 
+. . 7 . . 
+. . . . . 
+`;
+            case "image15":
+            case "Sign4":return img`
+. . . . . 
+. . . . . 
+7 7 7 7 7 
+. . . . . 
+7 7 7 7 7 
+. . . . . 
+. . . . . 
+`;
+            case "image17":
+            case "Down":return img`
+5 5 5 5 5 
+. 5 5 5 . 
+. . 5 . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+`;
+            case "image16":
+            case "Arrow":return img`
+. . . . . 
+. . . . . 
+. . . . . 
+. . 5 . . 
+. 5 5 5 . 
+5 5 5 5 5 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+5 5 5 5 5 
+. 5 5 5 . 
+. . 5 . . 
+. . . . . 
+. . . . . 
+. . . . . 
+`;
+            case "image18":
+            case "error":return img`
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+. . . . . 
+2 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
